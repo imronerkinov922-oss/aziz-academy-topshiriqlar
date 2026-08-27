a951172d7df240027b47e58a3cbfa80ce7c9d6f1
imronerkinov922-oss <imronerkinov922@gmail.com>
2026-08-27 11:10:09 +0500

    Loyiha: Free Fire o'yini — 91.5% (A'lo)

---
+++ loyihalar/13764-Free_Fire_o_yini/README.md
@@ -0,0 +1,6 @@
+# Free Fire o'yini Loyihasi
+
+Bu Loyiha Free Fire mobil o'yini haqidagi interaktiv veb - sahifadir.
+
+- HTML5 va CSS3 yordamida tuzilgan
+-JavaScript orqali interaktiv tugmalar qo'shilgan.
---
+++ loyihalar/13764-Free_Fire_o_yini/script.js
@@ -0,0 +1,13 @@
+/**
+* Tugma va xabar ko'rsatuvchi elementlarni olish
+*/
+const btn = document.getElementById('btn');
+const message = document.getElementById('message');
+
+/**
+* Tugma hodisa tinglovchisi qo'shish
+*/
+btn.addEventListener('click', function()) {
+    // Tugma bosilganda matnni o'zgartirish
+    message.textContent = "Free Fire o'yinig axush kelibsiz!"
+});        
