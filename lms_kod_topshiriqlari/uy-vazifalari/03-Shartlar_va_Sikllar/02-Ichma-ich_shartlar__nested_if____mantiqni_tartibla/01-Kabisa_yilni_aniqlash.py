31590986554c01211de3cf59b44d6e7385ccee06
imronerkinov922-oss <imronerkinov922@gmail.com>
2026-09-25 17:04:46 +0500

    Add: Kabisa yilni aniqlash [imronerkinov922-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Kabisa_yilni_aniqlash.py
@@ -0,0 +1,13 @@
+yil = int(input())
+if yil % 4 == 0:
+    if yil % 100 == 0:
+        if yil % 400 == 0:
+            print("Kabisa")
+        else:
+            print("Kabisa emas")
+    else:
+            print("Kabisa")
+else:
+            print("Kabisa emas")
+
+    
