98ad4616b3d931da5b8505ffd39b134ebeab9efc
imronerkinov922-oss <imronerkinov922@gmail.com>
2026-09-26 15:58:12 +0500

    Add: 10) Menyu: ob-havo [imronerkinov922-oss]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/10-10__Menyu__ob-havo.py
@@ -0,0 +1,11 @@
+weather = input().strip()
+
+if weather == "sunny":
+    print("Go out")
+elif weather == "rainy":
+    print("Stay home")
+else:
+    print("Unknown")
+# sunny -> "Go out"
+# rainy -> "Stay home"
+# aks holda "Unknown"
